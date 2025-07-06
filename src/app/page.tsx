e6e4e9f374291328927e6cb536851b32bbe04312
Pranshu Raj <rajpranshu2222@gmail.com>
2025-07-06 17:26:40 +0530

    redirect to home form "/ " route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { auth } from '@/lib/auth';
 import { useGlobalContext } from './providers/GlobalContext';
 import AuthGate from './components/auth/AuthGate';
+import { redirect, RedirectType } from 'next/navigation';
 
 export default async function () {
   const session = await auth();
+
+  redirect('/home', RedirectType.push);
 
   return <AuthGate session={session} />;
 }
